49acff0612db5ea7f036ee96778c369de8b37bbb
sanvika15 <137372400+sanvika15@users.noreply.github.com>
2024-06-03 13:47:08 +0530

    fixing issues

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -67,13 +67,13 @@ export default function TextForm(props) {
      <div className="container" style={{color: props.mode === 'dark'?'white':'black'}}>
          <h1>{props.heading} </h1>
          <div className="mb-3">
-         <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'?'grey':'white', color:props.mode === 'dark'?'white':'black'}} id="myBox" rows="8"></textarea>
+         <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode === 'dark'?'#13466e':'white', color:props.mode === 'dark'?'white':'black'}} id="myBox" rows="8"></textarea>
          </div>
-         <button className="btn btn-info mx-1" onClick={handleUpClick}> UpperCase</button>
-         <button className="btn btn-info mx-1" onClick={handleLoClick}> LowerCase</button>
-         <button className="btn btn-info mx-1" onClick={handleSeClick}> SentenceCase</button>
-         <button className="btn btn-info mx-1" onClick={handleCaClick}> CapitalizedCase</button>
-         <button className="btn btn-info mx-1" onClick={handleAtClick}> aTyicalCase</button>
+         <button className="btn btn-info mx-1 my-1" onClick={handleUpClick}> UpperCase</button>
+         <button className="btn btn-info mx-1 my-1" onClick={handleLoClick}> LowerCase</button>
+         <button className="btn btn-info mx-1 my-1" onClick={handleSeClick}> SentenceCase</button>
+         <button className="btn btn-info mx-1 my-1" onClick={handleCaClick}> CapitalizedCase</button>
+         <button className="btn btn-info mx-1 my-1" onClick={handleAtClick}> aTyicalCase</button>
 
          <button className="btn btn-info mx-1" onClick={handleClClick}>Clear Text</button>       
 
@@ -82,8 +82,8 @@ export default function TextForm(props) {
       </div>
       <div className="container my-3" style={{color: props.mode === 'dark'?'white':'black'}}>
         <h1>Your Text Summary</h1>
-        <p>{text.split(" ").length  } words and {text.length } characters</p>
-        <p>{0.008 * text.split(" ").length} Minutes read</p>
+        <p>{text.split(" ").filter((element)=>{return element.length!==0}).length  } words and {text.length } characters</p>
+        <p>{0.008 * text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
         <h2>Preview</h2>
         <p>{text.length>0?text:"Enter somethingin the textbox above to preview here"}</p>
       </div>
